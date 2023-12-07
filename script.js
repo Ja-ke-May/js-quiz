89edcb3c82bdd5368766f31b6abd62604985b97e
Ja-ke-May <142454161+Ja-ke-May@users.noreply.github.com>
2023-12-07 15:03:59 +0000

    instructions and end quiz div added with functionality

--- script.js
+++ script.js
@@ -9,161 +9,161 @@ const questions = [
           options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
           correctAnswer: "William Shakespeare",
         },
-        {
-          question: "In which year did the Titanic sink?",
-          options: ["1912", "1923", "1931", "1945"],
-          correctAnswer: "1912",
-        },
-        {
-          question: "What is the capital city of Japan?",
-          options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
-          correctAnswer: "Tokyo",
-        },
-        {
-          question: "Who is the author of 'To Kill a Mockingbird'?",
-          options: ["J.K. Rowling", "Harper Lee", "George Orwell", "Ernest Hemingway"],
-          correctAnswer: "Harper Lee",
-        },
-        {
-          question: "Which element has the chemical symbol 'O'?",
-          options: ["Oxygen", "Gold", "Osmium", "Iodine"],
-          correctAnswer: "Oxygen",
-        },
-        {
-          question: "Which mountain is the highest in the world?",
-          options: ["Mount Everest", "K2", "Makalu", "Lhotse"],
-          correctAnswer: "Mount Everest",
-        },
-        {
-          question: "What is the currency of Brazil?",
-          options: ["Peso", "Real", "Dollar", "Euro"],
-          correctAnswer: "Real",
-        },
-        {
-          question: "Who is known as the 'Father of Computer Science'?",
-          options: ["Alan Turing", "Bill Gates", "Steve Jobs", "Ada Lovelace"],
-          correctAnswer: "Alan Turing",
-        },
-        {
-          question: "In which year did the Berlin Wall fall?",
-          options: ["1989", "1991", "1977", "1995"],
-          correctAnswer: "1989",
-        },
-        {
-          question: "What is the largest mammal on Earth?",
-          options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
-          correctAnswer: "Blue Whale",
-        },
-        {
-          question: "Who painted the 'Mona Lisa'?",
-          options: ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
-          correctAnswer: "Leonardo da Vinci",
-        },
-        {
-          question: "Which gas do plants absorb from the atmosphere?",
-          options: ["Oxygen", "Carbon Monoxide", "Nitrogen", "Carbon Dioxide"],
-          correctAnswer: "Carbon Dioxide",
-        },
-        {
-          question: "What is the largest desert in the world?",
-          options: ["Sahara Desert", "Arabian Desert", "Gobi Desert", "Antarctica"],
-          correctAnswer: "Antarctica",
-        },
-        {
-          question: "Who discovered penicillin?",
-          options: ["Marie Curie", "Alexander Fleming", "Louis Pasteur", "Joseph Lister"],
-          correctAnswer: "Alexander Fleming",
-        },
-        {
-          question: "Which planet is known as the 'Morning Star'?",
-          options: ["Mercury", "Venus", "Mars", "Jupiter"],
-          correctAnswer: "Venus",
-        },
-        {
-          question: "What is the largest ocean on Earth?",
-          options: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"],
-          correctAnswer: "Pacific Ocean",
-        },
-        {
-          question: "Who wrote the 'Iliad' and the 'Odyssey'?",
-          options: ["Homer", "Virgil", "Sophocles", "Aristotle"],
-          correctAnswer: "Homer",
-        },
-        {
-          question: "In which year did World War II end?",
-          options: ["1945", "1950", "1939", "1941"],
-          correctAnswer: "1945",
-        },
-        {
-          question: "What is the largest planet in our solar system?",
-          options: ["Earth", "Jupiter", "Saturn", "Neptune"],
-          correctAnswer: "Jupiter",
-        },
-        {
-          question: "Which country is known as the 'Land of the Rising Sun'?",
-          options: ["China", "South Korea", "Japan", "Vietnam"],
-          correctAnswer: "Japan",
-      },
-      {
-          question: "Who is the 16th President of the United States?",
-          options: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "John F. Kennedy"],
-          correctAnswer: "Abraham Lincoln",
-      },
-      {
-          question: "What is the largest continent on Earth?",
-          options: ["North America", "Europe", "Asia", "Australia"],
-          correctAnswer: "Asia",
-      },
-      {
-          question: "Who wrote 'Romeo and Juliet'?",
-          options: ["Charles Dickens", "Jane Austen", "William Shakespeare", "Mark Twain"],
-          correctAnswer: "William Shakespeare",
-      },
-      {
-          question: "Which chemical element has the symbol 'H'?",
-          options: ["Helium", "Hydrogen", "Hassium", "Hafnium"],
-          correctAnswer: "Hydrogen",
-      },
-      {
-          question: "What is the capital city of France?",
-          options: ["Berlin", "Madrid", "Paris", "Rome"],
-          correctAnswer: "Paris",
-      },
-      {
-          question: "Who is the 44th President of the United States?",
-          options: ["Barack Obama", "Donald Trump", "George W. Bush", "Bill Clinton"],
-          correctAnswer: "Barack Obama",
-      },
-      {
-          question: "In which year was the Eiffel Tower completed?",
-          options: ["1889", "1901", "1923", "1945"],
-          correctAnswer: "1889",
-      },
-      {
-          question: "What is the capital city of Australia?",
-          options: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
-          correctAnswer: "Canberra",
-      },
-      {
-          question: "Who wrote '1984'?",
-          options: ["George Orwell", "Aldous Huxley", "Ray Bradbury", "Philip K. Dick"],
-          correctAnswer: "George Orwell",
-      },
-      {
-          question: "Who is the author of 'The Great Gatsby'?",
-          options: ["F. Scott Fitzgerald", "Ernest Hemingway", "Jane Austen", "Charles Dickens"],
-          correctAnswer: "F. Scott Fitzgerald",
-      },
-      {
-          question: "What is the largest river in the world?",
-          options: ["Amazon River", "Nile River", "Yangtze River", "Mississippi River"],
-          correctAnswer: "Amazon River",
-      },
-      {
-        question: "Who developed the theory of general relativity?",
-        options: ["Isaac Newton", "Albert Einstein", "Niels Bohr", "Galileo Galilei"],
-        correctAnswer: "Albert Einstein",
-      }
+      //   {
+      //     question: "In which year did the Titanic sink?",
+      //     options: ["1912", "1923", "1931", "1945"],
+      //     correctAnswer: "1912",
+      //   },
+      //   {
+      //     question: "What is the capital city of Japan?",
+      //     options: ["Beijing", "Seoul", "Tokyo", "Bangkok"],
+      //     correctAnswer: "Tokyo",
+      //   },
+      //   {
+      //     question: "Who is the author of 'To Kill a Mockingbird'?",
+      //     options: ["J.K. Rowling", "Harper Lee", "George Orwell", "Ernest Hemingway"],
+      //     correctAnswer: "Harper Lee",
+      //   },
+      //   {
+      //     question: "Which element has the chemical symbol 'O'?",
+      //     options: ["Oxygen", "Gold", "Osmium", "Iodine"],
+      //     correctAnswer: "Oxygen",
+      //   },
+      //   {
+      //     question: "Which mountain is the highest in the world?",
+      //     options: ["Mount Everest", "K2", "Makalu", "Lhotse"],
+      //     correctAnswer: "Mount Everest",
+      //   },
+      //   {
+      //     question: "What is the currency of Brazil?",
+      //     options: ["Peso", "Real", "Dollar", "Euro"],
+      //     correctAnswer: "Real",
+      //   },
+      //   {
+      //     question: "Who is known as the 'Father of Computer Science'?",
+      //     options: ["Alan Turing", "Bill Gates", "Steve Jobs", "Ada Lovelace"],
+      //     correctAnswer: "Alan Turing",
+      //   },
+      //   {
+      //     question: "In which year did the Berlin Wall fall?",
+      //     options: ["1989", "1991", "1977", "1995"],
+      //     correctAnswer: "1989",
+      //   },
+      //   {
+      //     question: "What is the largest mammal on Earth?",
+      //     options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+      //     correctAnswer: "Blue Whale",
+      //   },
+      //   {
+      //     question: "Who painted the 'Mona Lisa'?",
+      //     options: ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Claude Monet"],
+      //     correctAnswer: "Leonardo da Vinci",
+      //   },
+      //   {
+      //     question: "Which gas do plants absorb from the atmosphere?",
+      //     options: ["Oxygen", "Carbon Monoxide", "Nitrogen", "Carbon Dioxide"],
+      //     correctAnswer: "Carbon Dioxide",
+      //   },
+      //   {
+      //     question: "What is the largest desert in the world?",
+      //     options: ["Sahara Desert", "Arabian Desert", "Gobi Desert", "Antarctica"],
+      //     correctAnswer: "Antarctica",
+      //   },
+      //   {
+      //     question: "Who discovered penicillin?",
+      //     options: ["Marie Curie", "Alexander Fleming", "Louis Pasteur", "Joseph Lister"],
+      //     correctAnswer: "Alexander Fleming",
+      //   },
+      //   {
+      //     question: "Which planet is known as the 'Morning Star'?",
+      //     options: ["Mercury", "Venus", "Mars", "Jupiter"],
+      //     correctAnswer: "Venus",
+      //   },
+      //   {
+      //     question: "What is the largest ocean on Earth?",
+      //     options: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"],
+      //     correctAnswer: "Pacific Ocean",
+      //   },
+      //   {
+      //     question: "Who wrote the 'Iliad' and the 'Odyssey'?",
+      //     options: ["Homer", "Virgil", "Sophocles", "Aristotle"],
+      //     correctAnswer: "Homer",
+      //   },
+      //   {
+      //     question: "In which year did World War II end?",
+      //     options: ["1945", "1950", "1939", "1941"],
+      //     correctAnswer: "1945",
+      //   },
+      //   {
+      //     question: "What is the largest planet in our solar system?",
+      //     options: ["Earth", "Jupiter", "Saturn", "Neptune"],
+      //     correctAnswer: "Jupiter",
+      //   },
+      //   {
+      //     question: "Which country is known as the 'Land of the Rising Sun'?",
+      //     options: ["China", "South Korea", "Japan", "Vietnam"],
+      //     correctAnswer: "Japan",
+      // },
+      // {
+      //     question: "Who is the 16th President of the United States?",
+      //     options: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "John F. Kennedy"],
+      //     correctAnswer: "Abraham Lincoln",
+      // },
+      // {
+      //     question: "What is the largest continent on Earth?",
+      //     options: ["North America", "Europe", "Asia", "Australia"],
+      //     correctAnswer: "Asia",
+      // },
+      // {
+      //     question: "Who wrote 'Romeo and Juliet'?",
+      //     options: ["Charles Dickens", "Jane Austen", "William Shakespeare", "Mark Twain"],
+      //     correctAnswer: "William Shakespeare",
+      // },
+      // {
+      //     question: "Which chemical element has the symbol 'H'?",
+      //     options: ["Helium", "Hydrogen", "Hassium", "Hafnium"],
+      //     correctAnswer: "Hydrogen",
+      // },
+      // {
+      //     question: "What is the capital city of France?",
+      //     options: ["Berlin", "Madrid", "Paris", "Rome"],
+      //     correctAnswer: "Paris",
+      // },
+      // {
+      //     question: "Who is the 44th President of the United States?",
+      //     options: ["Barack Obama", "Donald Trump", "George W. Bush", "Bill Clinton"],
+      //     correctAnswer: "Barack Obama",
+      // },
+      // {
+      //     question: "In which year was the Eiffel Tower completed?",
+      //     options: ["1889", "1901", "1923", "1945"],
+      //     correctAnswer: "1889",
+      // },
+      // {
+      //     question: "What is the capital city of Australia?",
+      //     options: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+      //     correctAnswer: "Canberra",
+      // },
+      // {
+      //     question: "Who wrote '1984'?",
+      //     options: ["George Orwell", "Aldous Huxley", "Ray Bradbury", "Philip K. Dick"],
+      //     correctAnswer: "George Orwell",
+      // },
+      // {
+      //     question: "Who is the author of 'The Great Gatsby'?",
+      //     options: ["F. Scott Fitzgerald", "Ernest Hemingway", "Jane Austen", "Charles Dickens"],
+      //     correctAnswer: "F. Scott Fitzgerald",
+      // },
+      // {
+      //     question: "What is the largest river in the world?",
+      //     options: ["Amazon River", "Nile River", "Yangtze River", "Mississippi River"],
+      //     correctAnswer: "Amazon River",
+      // },
+      // {
+      //   question: "Who developed the theory of general relativity?",
+      //   options: ["Isaac Newton", "Albert Einstein", "Niels Bohr", "Galileo Galilei"],
+      //   correctAnswer: "Albert Einstein",
+      // }
 ];
 
 // Shuffle function, Fisher-Yates shuffle algorithm
@@ -186,6 +186,9 @@ const answerBoxs = document.getElementById('answer-boxs');
 const answerButtons = document.getElementsByClassName('answer-box');
 const plusThree = document.getElementById('green-plus');
 const minusFive = document.getElementById('red-minus');
+const finalScore = document.getElementById('final-score');
+const finalTime = document.getElementById('final-time');
+
 
 let currentQuestionIndex = 0;
 let count = 0;
@@ -239,11 +242,13 @@ const checkAnswer = (event) => {
   if (currentQuestionIndex < questions.length) {
     loadQuestion();
   } else {
-    // Display message 
-    alert('Quiz completed! Your score: ' + count);
-
-    // Reset the quiz when the alert "OK" is clicked
-    resetQuiz();
+    // Congratulations end quiz
+    endContainer.style.display = "block";
+      quizContainer.style.display = "none";
+    finalScore.textContent = count;
+    finalTime.textContent = timerDuration; 
+    endMessage.innerText = "Congratulations!"
+    endMessage1.innerText = "You've Completed all 33 questions!"
   }
 };
 
@@ -267,11 +272,18 @@ function startTimer() {
       updateTimer();
     } else {
       // Time is up, end the quiz
-      clearInterval(timer);
-      alert('Out of Time! Your score: ' + count);
-      resetQuiz();
+      endContainer.style.display = "block";
+      quizContainer.style.display = "none"; 
+      // Display final score and time
+    const endMessage = document.getElementById('end-message');
+    const endMessage1 = document.getElementById('end-message1');
+    finalScore.textContent = count;
+    finalTime.textContent = timerDuration;
+    endMessage.innerText = "Out of Time!"
+    endMessage1.innerText = ""
     }
   }, 1000);
+  count = 0;
 };
 
 // 3, 2, 1 timer red function
@@ -288,7 +300,6 @@ function resetTimer() {
   clearInterval(timer);
   timerDuration = 22; // Reset the timer duration
   updateTimer();
-  startTimer(); // Start the timer again after resetting
 };
 
 // Reset quiz and timer
@@ -299,12 +310,6 @@ function resetQuiz() {
   loadQuestion();
   resetTimer(); // Reset the timer when resetting the quiz
 };
-
-// Initial load of the first question and start timer
-loadQuestion();
-updateTimer(); // Display initial timer value
-startTimer(); // Start the timer
-
 
 // AUDIO
 const audio = document.querySelector('audio');
@@ -323,4 +328,79 @@ audioOff.addEventListener('click', () => {
   audioOn.style.display = "block";
   audioOff.style.display = "none";
   audio.play();
+});
+
+// Instructions
+
+// Welcome
+
+const welcomeContainer = document.getElementById('welcome-container');
+const welcomeNext = document.getElementById('welcome-next');
+
+welcomeNext.addEventListener('click', () => {
+  audioOn.style.display = "block";
+  audioOff.style.display = "none";
+   welcomeContainer.style.display = "none";
+   instruction1Container.style.display = "block";
+});
+
+// Instruction 1
+const instruction1Container = document.getElementById('instruction1-container');
+const instruction1Next = document.getElementById('instruction1-next');
+const instruction1Previous = document.getElementById('instruction1-previous');
+
+instruction1Next.addEventListener('click', () => {
+   instruction1Container.style.display = "none";
+   instruction2Container.style.display = "block";
+});
+
+instruction1Previous.addEventListener('click', () => {
+  instruction1Container.style.display = "none";
+  welcomeContainer.style.display = "block";
+});
+
+// Instruction 2
+
+const instruction2Container = document.getElementById('instruction2-container');
+const instruction2Next = document.getElementById('instruction2-next');
+const instruction2Previous = document.getElementById('instruction2-previous');
+
+instruction2Next.addEventListener('click', () => {
+   instruction2Container.style.display = "none";
+   startContainer.style.display = "block";
+});
+
+instruction2Previous.addEventListener('click', () => {
+  instruction2Container.style.display = "none";
+  instruction1Container.style.display = "block";
+});
+
+// Start container
+
+const startContainer = document.getElementById('start-container');
+const startNext = document.getElementById('start-next');
+const startPrevious = document.getElementById('start-previous');
+
+startNext.addEventListener('click', () => {
+   startContainer.style.display = "none";
+   quizContainer.style.display = "block";
+loadQuestion(); // Initial load of the first question and start timer
+updateTimer(); // Display initial timer value
+startTimer(); // Start the timer
+});
+
+startPrevious.addEventListener('click', () => {
+  startContainer.style.display = "none";
+  instruction2Container.style.display = "block";
+});
+
+// End Quiz
+
+const endContainer = document.getElementById('end-container');
+const endNext = document.getElementById('end-next');
+
+endNext.addEventListener('click', () => {
+   startContainer.style.display = "block";
+   endContainer.style.display = "none";
+resetQuiz();
 });
